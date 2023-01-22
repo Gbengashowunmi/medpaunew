a144861af3bb29d31e4379eef16bbe3ec38cb7c6
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2023-01-22 09:01:01 +0100

    slick-slide

--- src/component/CenterSlider.js
+++ src/component/CenterSlider.js
@@ -4,12 +4,37 @@ import ProductCard from "./productCard/ProductCard";
 
 export default function CenterSlider() {
   const settings = {
-    className: "center",
-    centerMode: true,
+    dots: false,
     infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
     speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+        responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <Slider {...settings}>
